7aaee7cccaa9ddb4b2feed81f765fc669babf923
syoch <hacksyoch@gmail.com>
2022-08-09 13:13:05 +0900

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,18 @@
 mod basic;
 mod v0101;
+
+#[cfg(test)]
+mod test {
+    use std::io::Read;
+
+    use super::*;
+
+    #[test]
+    fn test_7fs() {
+        let mut file = std::fs::File::open("test.7fs").unwrap();
+        let mut buf = Vec::<u8>::new();
+        file.read_to_end(&mut buf).unwrap();
+        let buf = &buf[16..];
+        println!("{:#?}", v0101::item(buf).unwrap().1);
+    }
+}
--- src/v0101/mod.rs
+++ src/v0101/mod.rs
@@ -1,2 +1,6 @@
 mod item;
 mod item_data;
+
+pub use item::item;
+pub use item::Item;
+pub use item_data::ItemData;
